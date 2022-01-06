c02c8766e408455f094388f6750fc286884b8642
kitarp29 <kitarpsinghrajpoot@gmail.com>
2022-01-06 15:34:36 +0530

    hiding bd url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import router from './routes.js'
 import mongoose from 'mongoose'
 import cors from 'cors'
 const app=express()
-mongoose.connect("mongodb+srv://GDSC:Pratik2902@cluster0.vsofg.mongodb.net/timetable?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
